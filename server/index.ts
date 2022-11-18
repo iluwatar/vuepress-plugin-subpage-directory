51cad693e6e5d46af8bf9680956cd7a205602f9c
Ilkka Seppälä <ilkka.seppala@gofore.com>
2022-11-18 20:43:40 +0200

    log links

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,11 @@ import type { PluginFunction } from '@vuepress/core'
 export let subpageDirectoryPlugin = (): PluginFunction => () => ({
   name: 'vuepress-plugin-subpage-directory',
   onPrepared(app) {
-    console.log(JSON.stringify(app.pages, null, 2))
+    let links = new Array<string>()
+    app.pages.forEach((page) => {
+      links.push(page.path)
+    })
+    console.log('Outputting all links')
+    console.log(links)
   },
 })
