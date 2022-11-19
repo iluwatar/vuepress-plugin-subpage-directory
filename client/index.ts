912ec0d8e4e502b3eab1061b30f3dcecf54422d1
Ilkka Seppälä <ilkka.seppala@gofore.com>
2022-11-19 20:12:05 +0200

    Revert "define simple string"
    This reverts commit e7e826aca01804b97efdea10456ff108743270b0.

--- client/index.ts
+++ client/index.ts
@@ -1,3 +1,6 @@
-declare const __SUBPAGELINKS__: string;
-
+export interface SubpageLink {
+    path: string
+}
+  
+declare const __SUBPAGELINKS__: SubpageLink[];
 export const subpageLinks = __SUBPAGELINKS__;
--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ export let subpageDirectoryPlugin = (): PluginFunction => (app: App) => (
   {
     name: 'vuepress-plugin-subpage-directory',
     define: {
-      __SUBPAGELINKS__: 'foo',
+      __SUBPAGELINKS__: app.pages.map((page) => {console.log(page.path); return page.path;}),
     },
   }
 )
